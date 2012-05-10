bab7222fed42e8a2537b15d82e618c0959852c35
Josef Jelinek <josef.jelinek@gmail.com>
2012-05-11 00:55:24 +0200

    making bb map default, starting vector

--- lang/feat.js
+++ lang/feat.js
@@ -7,22 +7,22 @@ var FEAT = (function (nil) {
   var is_own = function (o, key) { return Object.prototype.hasOwnProperty.call(o, key); };
   var freeze_object = Object.freeze || function (o) { return o; };
 
-  var map_node = function (key, val, lo, hi) {
+  var new_node = function (key, val, lev, lo, hi) {
     var count = 1 + (lo ? lo.count : 0) + (hi ? hi.count : 0);
-    var depth = 1 + Math.max(lo ? lo.depth : 0, hi ? hi.depth : 0);
-    return freeze_object({ key: key, val: val, lo: lo, hi: hi, count: count, depth: depth });
+    return freeze_object({ key: key, val: val, lev: lev, lo: lo, hi: hi, count: count });
   };
 
-  var map_with_lo_hi = function (node, lo, hi) { return node.lo === lo && node.hi === hi ? node : map_node(node.key, node.val, lo, hi); };
-  var map_with_lo = function (node, lo) { return map_with_lo_hi(node, lo, node.hi); };
-  var map_with_hi = function (node, hi) { return map_with_lo_hi(node, node.lo, hi); };
-  var map_go_lo = function (node, key, lt) { return lt && lt(key, node.key) || !lt && key < node.key; };
+  var with_lev = function (node, lev) { return new_node(node.key, node.val, lev, node.lo, node.hi); };
+  var with_lo_hi = function (node, lo, hi) { return new_node(node.key, node.val, node.lev, lo, hi); };
+  var with_lo = function (node, lo) { return lo === node.lo ? node : with_lo_hi(node, lo, node.hi); };
+  var with_hi = function (node, hi) { return hi === node.hi ? node : with_lo_hi(node, node.lo, hi); };
+  var go_lo = function (node, key, lt) { return lt && lt(key, node.key) || !lt && key < node.key; };
 
-  var map_has = function (node, key, lt) {
+  var has = function (node, key, lt) {
     while (node) {
       if (key === node.key)
         return true;
-      node = map_go_lo(node, key, lt) ? node.lo : node.hi;
+      node = go_lo(node, key, lt) ? node.lo : node.hi;
     }
     return false;
   };
@@ -31,95 +31,87 @@ var FEAT = (function (nil) {
     while (node) {
       if (key === node.key)
         return node.val;
-      node = map_go_lo(node, key, lt) ? node.lo : node.hi;
+      node = go_lo(node, key, lt) ? node.lo : node.hi;
     }
     return fail;
   };
 
-  var map_put = function (node, key, val, lt) {
+  var put = function (node, key, val, lt) {
     if (!node)
-      return map_node(key, val);
-    if (key === node.key && val === node.val)
-      return node;
-    var lo = node.lo, hi = node.hi;
+      return new_node(key, val, 0);
     if (key === node.key)
-      return map_node(key, val, lo, hi);
-    if (map_go_lo(node, key, lt)) {
-      var new_lo = map_put(lo, key, val, lt);
-      if (new_lo === lo)
-        return node;
-      if (new_lo.depth > (hi ? hi.depth + 1 : 1)) // rotate right
-        return map_with_hi(new_lo, map_with_lo(node, new_lo.hi));
-      return map_with_lo(node, new_lo);
-    }
-    var new_hi = map_put(hi, key, val, lt);
-    if (new_hi === hi)
-      return node;
-    if (new_hi.depth > (lo ? lo.depth + 1 : 1)) // rotate left
-      return map_with_lo(new_hi, map_with_hi(node, new_hi.lo));
-    return map_with_hi(node, new_hi);
+      return val === node.val ? node : new_node(key, val, node.lev, node.lo, node.hi);
+    node = go_lo(node, key, lt) ? skew(node, put(node.lo, key, val, lt)) : skew(with_hi(node, put(node.hi, key, val, lt)));
+    return split(node);
   };
 
-  var map_rm = function (node, key, lt) {
-    if (!node)
-      return node;
-    var lo = node.lo, hi = node.hi, new_hi, new_lo, hi_lo;
-    if (key === node.key) {
-      if (!lo || !hi)
-        return lo || hi;
-      for (hi_lo = hi; hi_lo.lo; hi_lo = hi_lo.lo) ; // find replacement
-      new_hi = map_rm(hi, hi_lo.key, lt);
-      if (lo && (new_hi ? new_hi.depth : 0) < lo.depth - 1) // rotate right
-        return map_with_hi(lo, map_with_lo_hi(hi_lo, lo.hi, new_hi));
-      return map_with_lo_hi(hi_lo, lo, new_hi);
-    }
-    if (map_go_lo(node, key, lt)) {
-      new_lo = map_rm(lo, key, lt);
-      if (new_lo === lo)
-        return node;
-      if (hi && (new_lo ? new_lo.depth : 0) < hi.depth - 1) // rotate left
-        return map_with_lo(hi, map_with_lo_hi(node, new_lo, hi.lo));
-      return map_with_lo(node, new_lo);
-    }
-    new_hi = map_rm(hi, key, lt);
-    if (new_hi === hi)
-      return node;
-    if (lo && (new_hi ? new_hi.depth : 0) < lo.depth - 1) // rotate right
-      return map_with_hi(lo, map_with_lo_hi(node, lo.hi, new_hi));
-    return map_with_hi(node, new_hi);
-  };
-
-  var map_keys = function (node, a) {
+  var rm = function (node, key, lt) {
     if (node) {
-      map_keys(node.lo, a);
+      var lo = node.lo, hi = node.hi, hi_lo, lev = node.lev;
+      if (key === node.key) {
+        if (!lo || !hi)
+          return lo || hi;
+        for (hi_lo = hi; hi_lo.lo; hi_lo = hi_lo.lo) ; // find replacement
+        node = new_node(hi_lo.key, hi_lo.val, lev, lo, hi = rm(hi, hi_lo.key, lt));
+      } else {
+        node = go_lo(node, key, lt) ? with_lo(node, lo = rm(lo, key, lt)) : with_hi(node, hi = rm(hi, key, lt));
+      }
+      if (lo && lo.lev < lev - 1 || hi && hi.lev < lev - 1) {
+        node = new_node(node.key, node.val, lev - 1, lo, hi && hi.lev > lev ? with_lev(hi, lev - 1) : hi);
+        node = skew(node);
+        if (node.hi)
+          node = with_hi(node, skew(node.hi));
+        if (node.hi && node.hi.hi)
+          node = with_hi(node, with_hi(node.hi, skew(node.hi.hi)));
+        node = split(node);
+        if (node.hi)
+          node = with_hi(node, split(node.hi));
+      }
+    }
+    return node;
+  };
+
+  var skew = function (node, lo) {
+    lo = lo || node.lo;
+    return !lo || node.lev > lo.lev ? with_lo(node, lo) : with_hi(lo, with_lo(node, lo.hi));
+  };
+
+  var split = function (node) {
+    var hi = node.hi;
+    return !hi || !hi.hi || node.lev > hi.hi.lev ? node : new_node(hi.key, hi.val, hi.lev + 1, with_hi(node, hi.lo), hi.hi);
+  };
+
+  var keys = function (node, a) {
+    if (node) {
+      keys(node.lo, a);
       a.push(node.key);
-      map_keys(node.hi, a);
+      keys(node.hi, a);
     }
     return a;
   };
 
-  var map_values = function (node, a) {
+  var values = function (node, a) {
     if (node) {
-      map_values(node.lo, a);
+      values(node.lo, a);
       a.push(node.val);
-      map_values(node.hi, a);
+      values(node.hi, a);
     }
     return a;
   };
 
-  var map_to_object = function (node, o) {
+  var to_object = function (node, o) {
     if (node) {
-      map_to_object(node.lo, o);
+      to_object(node.lo, o);
       o[node.key] = node.val;
-      map_to_object(node.hi, o);
+      to_object(node.hi, o);
     }
     return o;
   };
 
-  var map_print = function (node) {
+  var print_map = function (node) {
     if (!node)
       return '';
-    var s = map_print(node.lo), t = map_print(node.hi);
+    var s = print_map(node.lo), t = print_map(node.hi);
     if (s !== '')
       s = s + ','
     if (t !== '')
@@ -131,21 +123,20 @@ var FEAT = (function (nil) {
     if (!is_map(this))
       return new Map(node, lt);
     this.count = function () { return node ? node.count : 0; };
-    this.depth = function () { return node ? node.depth : 0; };
-    this.contains = function (key) { return map_has(node, key, lt); };
+    this.contains = function (key) { return has(node, key, lt); };
     this.get = function (key, fail) { return map_get(node, key, fail, lt); };
-    this.assoc = function (key, val) { return create_map_if_new(this, node, map_put(node, key, val, lt), lt); };
-    this.dissoc = function (key) { return create_map_if_new(this, node, map_rm(node, key, lt), lt); };
-    this.keys = function (into) { return map_keys(node, into || []); };
-    this.values = function (into) { return map_values(node, into || []); };
-    this.toObject = function (into) { return map_to_object(node, into || {}); };
-    this.toString = function () { return '{' + map_print(node) + ' }'; };
+    this.assoc = function (key, val) { return create_if_new(this, node, put(node, key, val, lt), lt); };
+    this.dissoc = function (key) { return create_if_new(this, node, rm(node, key, lt), lt); };
+    this.keys = function (into) { return keys(node, into || []); };
+    this.values = function (into) { return values(node, into || []); };
+    this.toObject = function (into) { return to_object(node, into || {}); };
+    this.toString = function () { return '{' + print_map(node) + ' }'; };
     freeze_object(this);
   };
 
   var is_map = function (x) { return x instanceof Map; };
 
-  var create_map_if_new = function (map, node, new_node, lt) {
+  var create_if_new = function (map, node, new_node, lt) {
     return node === new_node ? map : Map(new_node, lt);
   };
 
@@ -156,6 +147,9 @@ var FEAT = (function (nil) {
         map = map.assoc(key, obj[key]);
     return map;
   };
+
+
+  // Naive copy-on-write persistent map on top of the native object
 
   var nap_copy = function (obj) {
     var key, o = {};
@@ -195,10 +189,49 @@ var FEAT = (function (nil) {
     return Nap(nap_copy(obj));
   };
 
+
+  // Persistent vector based on a shallow native-array tree
+
+  var vec_get = function (node, i) {
+    return node ? node[i] : nil;
+  }
+
+  var push = function (node, val) {
+    var x = node.slice(0);
+    x.push(val);
+    return freeze_object(x);
+  };
+
+  var pop = function (node) {
+    return node.slice(0, -1);
+  };
+
+  var Vec = function (node, count) {
+    if (!is_vec(this))
+      return new Vec(node, count);
+    this.count = function () { return count; };
+    this.get = function (i) { return vec_get(node, i); };
+    this.push = function (val) { return Vec(push(node, val), count + 1); };
+    this.pop = function () { return node.length > 0 ? Vec(pop(node), count - 1) : this; };
+    this.toArray = function (into) { return to_array(node, into || []); };
+    this.toString = function () { return '{' + print_vec(node) + ' }'; };
+    freeze_object(this);
+  };
+
+  var is_vec = function (x) { return x instanceof Vec; };
+
+  var create_vec = function (arr) {
+    var i, len = arr ? arr.length : 0, vec = Vec([], 0);
+    for (i = 0; i < len; i++)
+      vec = vec.push(arr[i]);
+    return vec;
+  };
+
   return freeze_object({
     map: create_map,
     nap: create_nap,
     assoc: nap_assoc,
-    dissoc: nap_dissoc
+    dissoc: nap_dissoc,
+    vector: create_vec
   });
 }());
