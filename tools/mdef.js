aff9027d3434f5038a90cdbe692c3813951d0dcc
Josef Jelinek <josef.jelinek@gmail.com>
2013-05-07 02:01:38 +0200

    AMD inspired simple module system

---
+++ tools/mdef.js
@@ -0,0 +1,55 @@
+
+var MDEF = (function (nil) {
+    'use strict';
+    var defs, reqs, mods, reset, resolve, try_use;
+
+    reset = function () {
+        defs = {};
+        reqs = [];
+        mods = {};
+    };
+
+    resolve = function (mod_names) {
+        var i, name, res, sub_mods = [];
+        for (i = 0; i < mod_names.length; i += 1) {
+            name = mod_names[i];
+            if (!mods[name] && defs[name]) {
+                res = resolve(defs[name][1]);
+                if (res) {
+                    mods[name] = defs[name][0].apply(null, res);
+                    delete defs[name];
+                }
+            }
+            sub_mods[i] = mods[name];
+        }
+        return sub_mods.every(Boolean) ? sub_mods : null;
+    };
+
+    try_use = function () {
+        var i, new_reqs = [];
+        for (i = 0; i < reqs.length; i += 1) {
+            var res = resolve(reqs[i][1]);
+            if (res) {
+                reqs[i][0].apply(null, res);
+            } else {
+                new_reqs.push(reqs[i]);
+            }
+        }
+        reqs = new_reqs;
+    };
+
+    reset();
+    return {
+        reset: reset,
+
+        use: function (mod_names, fn) {
+            reqs.push([fn, mod_names])
+            try_use();
+        },
+
+        mod: function (name, mod_names, fn) {
+            defs[name] = defs[name] || [fn, mod_names];
+            try_use();
+        }
+    };
+}());
